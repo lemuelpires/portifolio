8cff1433620298e80d8b3ff82d728defc8aa093f
lemuelpires <107230579+lemuelpires@users.noreply.github.com>
2025-08-06 11:21:00 -0300

    ajuste no menu header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from './Header.module.css'; 
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Foto from '../../assets/foto/foto.jpg';
@@ -10,6 +10,23 @@ function Header() {
         setIsOpen(!isOpen);
     };
 
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+
+        // Cleanup
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, [isOpen]);
+
     return (
         <header className={style.header}>
             <div className={style.headerContainer}>
@@ -19,10 +36,10 @@ function Header() {
                 </div>
                 <nav className={`${style.headerNav} ${isOpen ? style.open : ''}`}>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/sobre"> Sobre</a></li>
-                        <li><a href="https://www.portalmantec.com.br" target="_blank" rel="noopener noreferrer">Projetos</a></li>
-                        <li><a href="/contato">Contato</a></li>
+                        <li><a href="/" onClick={closeMenu}>Home</a></li>
+                        <li><a href="/sobre" onClick={closeMenu}> Sobre</a></li>
+                        <li><a href="https://www.portalmantec.com.br" target="_blank" rel="noopener noreferrer" onClick={closeMenu}>Projetos</a></li>
+                        <li><a href="/contato" onClick={closeMenu}>Contato</a></li>
                     </ul>
                 </nav>
                 <div className={style.menuIcon} onClick={toggleMenu}>
